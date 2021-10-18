c427eb8c72e921d8c9ecec08139996bb5274ec70
Test Projects 83 <testprojects83@gmail.com>
2021-10-18 16:53:30 +0100

    added images for the platform

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,28 +1,29 @@
-import React from 'react'
-import './Banner.css'
+import React from 'react';
+import './Banner.css';
+import BannerImg from '.././../assets/BannerImg.png';
 
 const Banner = () => {
-    return (
-        <div>
-            <section class="banner center">
-                <div class="banner__content">
-                    <div class="banner-txt">
-                        <div class="hd">
-                            <h3>Fast-Track your</h3>
-                            <h3>Career with Hands-On</h3>
-                            <h3>Coding Courses</h3>
-                        </div>
-                        <div class="cta">
-                            <a href="coursePages/courses.html" class="cta-btn">Explore Courses</a>
-                        </div>
-                        <div class="banner__img">
-                            <img src="/assets/auto_img_1633698527005.png" alt="" />
-                        </div>
-                    </div>
-                </div>
-            </section>
+  return (
+    <section class='banner center'>
+      <div class='banner__content'>
+        <div class='banner-txt'>
+          <div class='hd'>
+            <h3>Fast-Track your</h3>
+            <h3>Career with Hands-On</h3>
+            <h3>Coding Courses</h3>
+          </div>
+          <div class='cta'>
+            <a href='coursePages/courses.html' class='cta-btn'>
+              Explore Courses
+            </a>
+          </div>
+          <div class='banner__img'>
+            <img src={BannerImg} alt='' />
+          </div>
         </div>
-    )
-}
+      </div>
+    </section>
+  );
+};
 
-export default Banner
+export default Banner;
